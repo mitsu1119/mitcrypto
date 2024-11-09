5cafb87e43287015bd8be71ab065dd554c482ef9
mitsu1119 <hachimitsu1119@gmail.com>
2024-11-09 17:49:42 +0900

    FiniteFieldElement に mul を実装

--- math/src/finite_field_element.rs
+++ math/src/finite_field_element.rs
@@ -1,7 +1,7 @@
 use core::panic;
 use std::{
     fmt::Display,
-    ops::{Add, AddAssign, Sub, SubAssign},
+    ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign},
 };
 
 use rug::Integer;
@@ -56,6 +56,20 @@ impl<'a> FiniteFieldElement<'a> {
             self.x += self.parent.order();
         }
     }
+
+    fn mul(&mut self, rhs: Self) {
+        if self.parent != rhs.parent {
+            panic!(
+                "{}",
+                MathError::unsupported_operand("*", self.parent, rhs.parent)
+            )
+        }
+
+        self.x *= rhs.x;
+        if &self.x >= self.parent.order() {
+            self.x %= self.parent.order();
+        }
+    }
 }
 
 impl<'a> AddAssign for FiniteFieldElement<'a> {
@@ -84,6 +98,21 @@ impl<'a> Sub for FiniteFieldElement<'a> {
     fn sub(self, rhs: Self) -> Self::Output {
         let mut res = self;
         res -= rhs;
+        res
+    }
+}
+
+impl<'a> MulAssign for FiniteFieldElement<'a> {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.mul(rhs);
+    }
+}
+
+impl<'a> Mul for FiniteFieldElement<'a> {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self::Output {
+        let mut res = self;
+        res *= rhs;
         res
     }
 }
@@ -170,5 +199,35 @@ mod tests {
         let fp2 = FiniteField::new(13.into()).unwrap();
 
         let _ = fp1.elem(3.into()) - fp2.elem(3.into());
+    }
+
+    #[test]
+    fn mul() {
+        let fp = FiniteField::new(11.into()).unwrap();
+        let tests = [
+            (fp.elem(3.into()), fp.elem(3.into())),
+            (fp.elem(10.into()), fp.elem(5.into())),
+            (fp.elem(100.into()), fp.elem(99.into())),
+            (fp.elem(3.into()), fp.elem((-20).into())),
+        ];
+        let res = [
+            fp.elem(9.into()),
+            fp.elem(6.into()),
+            fp.elem(0.into()),
+            fp.elem(6.into()),
+        ];
+
+        for (t, r) in tests.into_iter().zip(res) {
+            assert_eq!(t.0 * t.1, r);
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn mul_err() {
+        let fp1 = FiniteField::new(11.into()).unwrap();
+        let fp2 = FiniteField::new(13.into()).unwrap();
+
+        let _ = fp1.elem(3.into()) * fp2.elem(3.into());
     }
 }
