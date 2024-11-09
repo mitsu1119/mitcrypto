aeb21fa7369cb432a9724a258f7e442d67fe68f5
mitsu1119 <hachimitsu1119@gmail.com>
2024-11-09 19:08:48 +0900

    zmod の実装

--- math/src/lib.rs
+++ math/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod error;
 pub mod finite_field;
+pub mod mod_ring;
---
+++ math/src/mod_ring/mod_ring.rs
@@ -0,0 +1,67 @@
+use std::fmt::Display;
+
+use rug::Integer;
+
+use crate::error::MathError;
+
+type Result<T> = std::result::Result<T, MathError>;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Zmod {
+    order: Integer,
+}
+
+impl Zmod {
+    pub fn new(order: Integer) -> Result<Self> {
+        if order == 0 {
+            Err(MathError::ValueError(
+                "the order of Zmod must be not zero".into(),
+            ))
+        } else {
+            Ok(Zmod { order: order.abs() })
+        }
+    }
+
+    pub fn order(&self) -> &Integer {
+        &self.order
+    }
+}
+
+impl Display for Zmod {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Ring of integers modulo {}", self.order())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::error::MathError;
+
+    use rug::Integer;
+
+    use super::Zmod;
+
+    #[test]
+    fn zmod() {
+        let tests = [
+            Zmod::new(Integer::from(5)),
+            Zmod::new(Integer::from(-5)),
+            Zmod::new(Integer::from(0)),
+        ];
+        let res = [
+            Ok(Zmod {
+                order: Integer::from(5),
+            }),
+            Ok(Zmod {
+                order: Integer::from(5),
+            }),
+            Err(MathError::ValueError(
+                "the order of Zmod must be not zero".into(),
+            )),
+        ];
+
+        for (t, r) in tests.iter().zip(&res) {
+            assert_eq!(t, r);
+        }
+    }
+}
