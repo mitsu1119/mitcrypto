438659a631b6b7777856b916520d97060b26ab61
mitsu1119 <hachimitsu1119@gmail.com>
2024-11-17 18:04:33 +0900

    メッセージのパースを実装

--- hash/src/sha1.rs
+++ hash/src/sha1.rs
@@ -1,15 +1,23 @@
 pub struct Sha1 {}
 
 impl Sha1 {
-    const BLOCK_SIZE: usize = 512;
-    const WORD_SIZE: usize = 32;
-    const DIGEST_SIZE: usize = 160;
+    // bytes
+    const BLOCK_SIZE: usize = 64;
+    const WORD_SIZE: usize = 4;
+    const DIGEST_SIZE: usize = 20;
+    const IV: [[u8; Self::WORD_SIZE]; Self::DIGEST_SIZE / Self::WORD_SIZE] = [
+        [0x67, 0x45, 0x23, 0x01],
+        [0xef, 0xcd, 0xab, 0x89],
+        [0x98, 0xba, 0xdc, 0xfe],
+        [0x10, 0x32, 0x54, 0x76],
+        [0xc3, 0xd2, 0xe1, 0xf0],
+    ];
 
     fn pad(m: Vec<u8>) -> Vec<u8> {
         let l = m.len();
-        let mods = (l + 1) % 64;
+        let mods = (l + 1) % Self::BLOCK_SIZE;
         let k = if mods > 56 {
-            64 - (mods - 56)
+            Self::BLOCK_SIZE - (mods - 56)
         } else if mods < 56 {
             56 - mods
         } else {
@@ -26,6 +34,17 @@ impl Sha1 {
         };
         res
     }
+
+    fn parse(m: Vec<u8>) -> Result<Vec<[u8; Self::BLOCK_SIZE]>, Vec<u8>> {
+        if m.len() % Self::BLOCK_SIZE != 0 {
+            return Err(m);
+        }
+        let mut res = vec![];
+        for block in m.chunks(Self::BLOCK_SIZE) {
+            res.push(block.try_into().unwrap());
+        }
+        Ok(res)
+    }
 }
 
 #[cfg(test)]
@@ -37,7 +56,7 @@ mod tests {
         let tests = [vec![0xaa; 100]];
 
         for v in tests {
-            println!("{:x?}", Sha1::pad(v));
+            println!("{:x?}", Sha1::parse(Sha1::pad(v)));
         }
         panic!();
     }
