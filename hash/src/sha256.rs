0767609451cc64fc85bcf568feccd69b384a6361
mitsu1119 <hachimitsu1119@gmail.com>
2024-12-03 11:12:59 +0900

    sha256の必須関数の実装

--- hash/src/sha256.rs
+++ hash/src/sha256.rs
@@ -34,6 +34,38 @@ impl Sha256 {
         0x5be0cd19,
     ];
 
+    #[allow(non_snake_case)]
+    #[inline(always)]
+    fn S0(x: u32) -> u32 {
+        x.rotate_right(2) ^ x.rotate_right(13) ^ x.rotate_right(22)
+    }
+
+    #[allow(non_snake_case)]
+    #[inline(always)]
+    fn S1(x: u32) -> u32 {
+        x.rotate_right(6) ^ x.rotate_right(11) ^ x.rotate_right(25)
+    }
+
+    #[inline(always)]
+    fn s0(x: u32) -> u32 {
+        x.rotate_right(7) ^ x.rotate_right(18) ^ (x >> 3)
+    }
+
+    #[inline(always)]
+    fn s1(x: u32) -> u32 {
+        x.rotate_right(17) ^ x.rotate_right(19) ^ (x >> 10)
+    }
+
+    #[inline(always)]
+    fn ch(x: u32, y: u32, z: u32) -> u32 {
+        (x & y) ^ ((!x) & z)
+    }
+
+    #[inline(always)]
+    fn maj(x: u32, y: u32, z: u32) -> u32 {
+        (x & y) ^ (x & z) ^ (y & z)
+    }
+
     fn pad(m: Vec<u8>) -> Vec<u8> {
         let l = m.len();
         let mods = (l + 1) % Self::BLOCK_SIZE;
